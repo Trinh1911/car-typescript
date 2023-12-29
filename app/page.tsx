efa50fdf67a378074755e414b12277597e744156
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-12-29 21:59:03 +0700

    2h27

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,44 @@
 import { Hero } from '@/components'
+import { CarCad } from '@/components'
 import { SearchBar } from '@/components'
 import { CustomFilter } from '@/components'
+import { fetchCars } from '@/untils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home({searchParams}) {
+  const allCars = await fetchCars(
+    {
+      manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+    }
+  )
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
-      <Hero/>
+      <Hero />
       <div className='mt-12 padding-x padding-y max-width' id="discover">
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catologue</h1>
           <p>Explore the cars you might like</p>
         </div>
         <div className='home__filters'>
-          <SearchBar/>
+          <SearchBar />
           <div className='home__filter-container'>
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? <section>
+          <div className='home__car-wrapper'>
+            {allCars?.map((car) => <CarCad car={car} />)}
+          </div>
+        </section> : (
+          <div className='home__error-container'><h2 className='text-black text-xl font-bold'>no result</h2>
+            <p>{allCars?.message}</p></div>
+        )}
       </div>
     </main>
   )
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import CustomButton from "./CustomButton";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCad from "./CarCad";
+import CarDetails from "./CarDetails";
 export {
-    Hero, CustomButton, Footer, NavBar, SearchBar, CustomFilter, SearchManufacturer
+    Hero, CustomButton, Footer, NavBar, SearchBar, CustomFilter, SearchManufacturer, CarCad, CarDetails
 }
